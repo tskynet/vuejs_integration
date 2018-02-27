98cbb7ebfdb1706e682b1b2c98965fdd402fbb4d
tskynet <stephdumonceau@gmail.com>
2018-02-27 23:29:06 +0100

    work on Notification component

--- src/components/UI/Notification/Notification.vue
+++ src/components/UI/Notification/Notification.vue
@@ -13,12 +13,38 @@
               <span>This is a plain notification</span>
             </div>
             <div class="alert alert-info">
-              <button class="close" type="button" aria-hidden="true">x</button>
+              <button class="close" type="button" aria-hidden="true">×</button>
               <span>This is a notification with close button.</span>
+            </div>
+            <div class="alert alert-info alert-icon">
+              <button class="close" type="button" aria-hidden="true">×</button>
+              <span class="ti-bell icon-notif"></span>
+              <span>This is a notification with close button and icon.</span>
+            </div>
+            <div class="alert alert-info alert-icon">
+              <button class="close" type="button" aria-hidden="true">×</button>
+              <span class="ti-pie-chart icon-notif"></span>
+              <span>This is a notification with close button and icon and have many lines. You can see that the icon and the close button are always vertically aligned. This is a beautiful notification. So you don't have to worry about the style.</span>
             </div>
           </b-col>
           <b-col md="6">
             <h5>Notifications states</h5>
+            <div class="alert alert-info">
+              <button class="close" type="button" aria-hidden="true">×</button>
+              <span><b>Info</b> - This is a regular notification made with ".alert-info"</span>
+            </div>
+            <div class="alert alert-success">
+              <button class="close" type="button" aria-hidden="true">×</button>
+              <span><b>Success</b> - This is a regular notification made with ".alert-success"</span>
+            </div>
+            <div class="alert alert-warning">
+              <button class="close" type="button" aria-hidden="true">×</button>
+              <span><b>Warning</b> - This is a regular notification made with ".alert-warning"</span>
+            </div>
+            <div class="alert alert-danger">
+              <button class="close" type="button" aria-hidden="true">×</button>
+              <span><b>Danger</b> - This is a regular notification made with ".alert-danger"</span>
+            </div>
           </b-col>
         </b-row>
       </div>
@@ -34,6 +60,16 @@ export default {
 }
 </script>
 <style scoped>
+#notification{
+  padding:30px;
+}
+#notification .contentNotif{
+  background:white;
+  border-radius:6px;
+}
+#notification .contentNotif .headerNotif{
+  padding: 15px 15px 10px 15px;
+}
 #notification .contentNotif .headerNotif h4{
   color:rgb(37,36,34);
   font-size:21px;
@@ -44,9 +80,62 @@ export default {
   font-size:14px;
   font-weight:400;
 }
+#notification .contentNotif .containerNotif{
+  padding: 15px 15px 10px 15px;
+}
+#notification .contentNotif .containerNotif h5{
+  color:rgb(37,36,34);
+  font-size: 17.5px;
+  font-weight: 400;
+  line-height:1.4em;
+  margin-bottom:15px;
+}
 #notification .contentNotif .containerNotif span{
-  color:rgb(48,145,178);
   font-size:14px;
   line-height:20px;
+  max-width:89%;
+  display:block;
+  box-sizing:border-box;
 }
+#notification .close{
+  float:right;
+  font-size:21px;
+  font-weight:bold;
+  line-height:1;
+  color:black;
+  text-shadow:  0 1px 0 white;
+  opacity: 0.2;
+}
+#notification .alert{
+  padding: 10px 15px 10px 15px;
+  margin-bottom:20px;
+  border-radius:4px;
+}
+#notification .alert-info{
+  background-color: #7CE4FE;
+  color: #3091B2;
+}
+#notification .alert-icon{
+  padding-left:65px;
+}
+#notification .alert-success{
+  background-color:#8EF3C5;
+  color:#229863;
+}
+#notification .alert-warning{
+  background-color:#FFE28C;
+  color:#BB992F;
+}
+#notification .alert-danger{
+  background-color:#FF8F5E;;
+  color:#B33C12;
+}
+#notification .icon-notif{
+  position:absolute;
+  left: 15px;
+  top: 50%;
+  margin-top:-10px;
+  font-size:30px !important;
+}
+
 </style>
