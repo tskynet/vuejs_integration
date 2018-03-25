06e3b523769756de0ac0f85b9793f659275c109f
tskynet <stephdumonceau@gmail.com>
2018-03-25 13:44:42 +0200

    fix style of popup notification

--- src/components/UI/PluginNotification/PluginNotification.vue
+++ src/components/UI/PluginNotification/PluginNotification.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="pluginNotification">
-    <div class="content">
-
+    <div class="content alert-info">
+      <button class="close" type="button" aria-hidden="true">×</button>
       <b-row>
         <b-col md="2">
-
+          <i class="ti-gift"></i>
         </b-col>
         <b-col md="10">
           <div class="test">Welcome to <b>Paper Dashboard</b> - a beautiful freebie for every web developer.</div>
@@ -23,16 +23,15 @@ export default {
 </script>
 <style scoped>
 #pluginNotification{
-  padding: 10px 10px 10px 20px;
-  width: 350px;
-
 }
 #pluginNotification .content{
-
-  height:60px;
-  background-color:rgb(34, 152, 99);
+  font-size:14px;
+  height:70px;
   border-radius:4px;
   box-sizing:border-box;
+  padding: 10px 10px 10px 10px;
+  width: 350px;
+  position:relative;
 }
 #pluginNotification .content .row{
   width:100%;
@@ -40,8 +39,20 @@ export default {
 }
 #pluginNotification .content .row .test{
   display:block;
-  font-size:14px;
-
-
+}
+#pluginNotification .content .row i{
+  font-size:30px;
+}
+#pluginNotification .alert-info{
+  background:rgb(124,228,254);
+  color:#3091B2;
+}
+#pluginNotification .close{
+  position: absolute;
+  top:5px;
+  right:10px;
+  text-shadow:0 1px 0 #fff;
+  opacity: .2;
+  color:#000;
 }
 </style>
